2a2f12fbeb8f34d26a5526676ad3bd3d5846a497
Brett Witty <brettwitty@brettwitty.net>
2020-01-14 21:11:05 +1100

    Add nicer ship image
    Same wasteful version as before, but it's not too expensive.

--- ship.py
+++ ship.py
@@ -1,11 +1,12 @@
 import pygame
+import numpy as np
 
 import physics
 import constants
 
 class Ship(physics.PhysicsObject):
 
-    def __init__(self, x=0.0, y=0.0, vx=0.0, vy=0.0, angle=90.0):
+    def __init__(self, x=0.0, y=0.0, vx=0.0, vy=0.0, angle=0.0):
 
         super().__init__(x=x, y=y, vx=vx, vy=vy, angle=angle)
 
@@ -21,8 +22,8 @@ class Ship(physics.PhysicsObject):
         self.rect = pygame.Rect(x,y,32,32)
         self.visible = 1
 
-        self.base_image = pygame.Surface([32,32], flags=pygame.SRCALPHA)
-        self.base_image_thrust = pygame.Surface([32,32], flags=pygame.SRCALPHA)
+        self.base_images = [pygame.Surface([32,32], flags=pygame.SRCALPHA) for i in range(360) ]
+        self.base_images_thrust = [pygame.Surface([32,32], flags=pygame.SRCALPHA) for i in range(360) ]
         self.mask = pygame.Mask([32,32])
         self.init_base_image()
 
@@ -32,25 +33,33 @@ class Ship(physics.PhysicsObject):
 
     def init_base_image(self):
 
-        peak = (28,16)
-        right = (4,28)
-        left = (4,4)
-        butt = (8,16)
+        for theta in range(360):
 
-        pygame.draw.aalines(self.base_image, (255,255,255), True, [peak, right, butt, left])
-        pygame.draw.aalines(self.base_image, (255,255,255), True, [peak, right, butt, left])
+            peak = pygame.Vector2(12,0)
+            right = pygame.Vector2(-12,12)
+            left = pygame.Vector2(-12,-12)
+            butt = pygame.Vector2(-8,0)
 
-        left_thruster = (6,6)
-        mid_thruster = (1,16)
-        right_thruster = (6,22)
+            left_thruster = pygame.Vector2(-10,-10)
+            mid_thruster = pygame.Vector2(-16,0)
+            right_thruster = pygame.Vector2(-10,10)
 
-        pygame.draw.aalines(self.base_image_thrust, (255,255,255), True, [peak, right, butt, left])
-        pygame.draw.aalines(self.base_image_thrust, (255,255,255), True, [peak, right, butt, left])
-        pygame.draw.aalines(self.base_image_thrust, pygame.Color("#ff8c00ff"), False, [left_thruster,mid_thruster, right_thruster])
-        pygame.draw.aalines(self.base_image_thrust, pygame.Color("#ff8c00ff"), False, [left_thruster,mid_thruster, right_thruster])
-        pygame.draw.aaline(self.base_image_thrust, pygame.Color("#ff8c00ff"), butt, mid_thruster)
+            for c in [peak, right, left, butt, left_thruster, mid_thruster, right_thruster]:
+                c.rotate_ip(-theta)
+                c += (16,16)
 
-        self.image = self.base_image.copy()
+            # Non thrusting image
+            pygame.draw.aalines(self.base_images[theta], (255,255,255), True, [peak, right, butt, left])
+            pygame.draw.aalines(self.base_images[theta], (255,255,255), True, [peak, right, butt, left])
+                        
+            # Thrusting image
+            pygame.draw.aalines(self.base_images_thrust[theta], (255,255,255), True, [peak, right, butt, left])
+            pygame.draw.aalines(self.base_images_thrust[theta], (255,255,255), True, [peak, right, butt, left])
+            pygame.draw.aalines(self.base_images_thrust[theta], pygame.Color("#ff8c00ff"), False, [left_thruster,mid_thruster, right_thruster])
+            pygame.draw.aalines(self.base_images_thrust[theta], pygame.Color("#ff8c00ff"), False, [left_thruster,mid_thruster, right_thruster])
+            pygame.draw.aaline(self.base_images_thrust[theta], pygame.Color("#ff8c00ff"), butt, mid_thruster)
+
+        self.image = self.base_images[int(self.angle)]
 
     def update(self, tick):
 
@@ -58,22 +67,20 @@ class Ship(physics.PhysicsObject):
 
         self.firing_cooldown -= tick
 
+        idx = int(np.round(self.angle)) % 360
+
         if self.firing and self.firing_cooldown <= 0 :
             self.firing_cooldown = constants.FIRE_COOLDOWN
             pygame.event.post(pygame.event.Event(constants.FIRE_EVENT))
 
         if self.thrusting:
-            rotated_img = pygame.transform.rotate( self.base_image_thrust, self.angle)
+            self.image = self.base_images_thrust[idx]
             if not self.thrust_channel.get_busy():
                 self.thrust_channel.play(self.thrust_sound, loops=-1)
         else:
-            rotated_img = pygame.transform.rotate( self.base_image, self.angle)
+            self.image = self.base_images[idx]
             if self.thrust_channel.get_busy():
                 self.thrust_channel.fadeout(constants.FADE_OUT_TIME)
-        rotated_rect = self.rect.copy()
-        rotated_rect.center = rotated_img.get_rect().center
-        rotated_img = rotated_img.subsurface(rotated_rect).copy()
-        self.image = rotated_img
         self.dirty = 1
 
     def thrust(self, tick):
